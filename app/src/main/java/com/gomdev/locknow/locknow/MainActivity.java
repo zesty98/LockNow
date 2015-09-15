ca7479a1573b3a8ad6576add61aed33f10f998d5
gomdev <gomdev@naver.com>
2015-09-16 00:05:55 +0900

    lock now initial update

---
+++ app/src/main/java/com/gomdev/locknow/locknow/MainActivity.java
@@ -0,0 +1,108 @@
+package com.gomdev.locknow.locknow;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private DevicePolicyManager mDPM;
+    private boolean mAdminActive = false;
+    private ComponentName mComponentName = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+
+        mComponentName = new ComponentName(this, DeviceAdminSampleReceiver.class);
+
+        mDPM = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+
+        mAdminActive = mDPM.isAdminActive(mComponentName);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mAdminActive = mDPM.isAdminActive(mComponentName);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (mAdminActive == true) {
+            mDPM.lockNow();
+        }
+    }
+
+
+    public static class DeviceAdminSampleReceiver extends DeviceAdminReceiver {
+        void showToast(Context context, String msg) {
+            String status = context.getString(R.string.admin_receiver_status, msg);
+            Toast.makeText(context, status, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction() == ACTION_DEVICE_ADMIN_DISABLE_REQUESTED) {
+                abortBroadcast();
+            }
+            super.onReceive(context, intent);
+        }
+
+        @Override
+        public void onEnabled(Context context, Intent intent) {
+            showToast(context, context.getString(R.string.admin_receiver_status_enabled));
+        }
+
+        @Override
+        public CharSequence onDisableRequested(Context context, Intent intent) {
+            return context.getString(R.string.admin_receiver_status_disable_warning);
+        }
+
+        @Override
+        public void onDisabled(Context context, Intent intent) {
+            showToast(context, context.getString(R.string.admin_receiver_status_disabled));
+        }
+    }
+}
